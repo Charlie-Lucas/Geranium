b7321784577f4b916807c76547d1d25fe4cf938b
Charlie Lucas <charlielucas.cl@gmail.com>
2021-06-28 14:37:25 +0200

    remove dead code

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -13,32 +13,4 @@ use Symfony\Component\Routing\Annotation\Route;
 class SecurityController extends AbstractController
 {
 
-    /**
-     * @Route("/api/logirgregdefg", name="app_login", methods={"POST"})
-
-    public function login(Request $request): Response
-    {
-        if(!$this->isGranted('IS_AUTHENTICATED_FULLY')) {
-            return $this->json([
-                "error" => "invalid login request"
-                ], 400);
-
-        }
-
-        $user = $this->getUser();
-
-        return $this->json([
-            'username' => $user->getUserIdentifier(),
-            'roles' => $user->getRoles(),
-        ]);
-    }
-
-    /**
-     * @Route("/api/logout", name="app_logout")
-     * @throws Exception
-
-    public function logout(Request $request)
-    {
-        throw new Exception('should not be reached');
-    }*/
 }
